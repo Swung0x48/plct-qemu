4b58a036cd932217718efcf4d8a08f825b3a009c
liweiwei <liweiwei@iscas.ac.cn>
2021-07-29 17:32:47 +0800

    add support for svinval extension

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -908,6 +908,7 @@ static bool gen_unary(DisasContext *ctx, arg_r2 *a,
 #include "insn_trans/trans_rvv.c.inc"
 #include "insn_trans/trans_rvb.c.inc"
 #include "insn_trans/trans_privileged.c.inc"
+#include "insn_trans/trans_svinval.c.inc"
 
 /* Include the auto-generated decoder for 16 bit insn */
 #include "decode-insn16.c.inc"
