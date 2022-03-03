300ac51a6a001701cd316e7467531f8dc54fc4af
Weiwei Li <liweiwei@iscas.ac.cn>
2022-03-03 21:23:29 +0800

    target/riscv: export property flags for xpulp
    Set the default value to false
    
    Signed-off-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Signed-off-by: Junqiang Wang <wangjunqiang@iscas.ac.cn>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1041,6 +1041,7 @@ static Property riscv_cpu_extensions[] = {
 
     /* Vendor-specific custom extensions */
     DEFINE_PROP_BOOL("xventanacondops", RISCVCPU, cfg.ext_XVentanaCondOps, false),
+    DEFINE_PROP_BOOL("xpulp", RISCVCPU, cfg.ext_XPulp, false),
 
     /* These are experimental so mark with 'x-' */
     DEFINE_PROP_BOOL("x-j", RISCVCPU, cfg.ext_j, false),
