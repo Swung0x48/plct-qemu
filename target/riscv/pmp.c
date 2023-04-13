44f896ad220b24a3599fb01421637ad1125de8b4
Weiwei Li <liweiwei@iscas.ac.cn>
2023-04-13 11:50:09 +0800

    target/riscv: Move pmp_get_tlb_size apart from get_physical_address_pmp
    pmp_get_tlb_size can be separated from get_physical_address_pmp and is only
    needed when ret == TRANSLATE_SUCCESS.
    
    Signed-off-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Signed-off-by: Junqiang Wang <wangjunqiang@iscas.ac.cn>

--- target/riscv/pmp.c
+++ target/riscv/pmp.c
@@ -612,6 +612,10 @@ target_ulong pmp_get_tlb_size(CPURISCVState *env, target_ulong addr)
     target_ulong tlb_ea = tlb_sa + TARGET_PAGE_SIZE - 1;
     int i;
 
+    if (!riscv_cpu_cfg(env)->pmp || !pmp_get_num_rules(env)) {
+        return TARGET_PAGE_SIZE;
+    }
+
     for (i = 0; i < MAX_RISCV_PMPS; i++) {
         pmp_sa = env->pmp_state.addr[i].sa;
         pmp_ea = env->pmp_state.addr[i].ea;
